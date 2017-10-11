2aa27abbb69c3c714c5f23324794ebde59b50ec0
Elizabeth Kim <eli.kim@mail.utoronto.ca>
2017-10-11 15:47:18 -0400

    sort table of Forward_Sortation_Area in decreasing number of incidents for each postal code

--- processALLyears.sh
+++ processALLyears.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+#assignment3
+#processALLyears.sh
+
 for i in *csv; do 
+
+#process Rscript processTPS.R for all years
+
 	Rscript processTPS.R $i 
 	echo ""   
 done
--- processTPS.R
+++ processTPS.R
@@ -1,33 +1,38 @@
 # assignment 3
+#processTPS.R
 
 # read command line argument
 args <- commandArgs(trailingOnly = TRUE)
-cat("Processing data from file:", args, "\n")
 
 
 # load csv file into data frame
 TPSdata <- read.csv(args)
 
 
+# cat "Processing data from file:" with the csv file
+cat("Processing data from file:", args, "\n")
 
+# cat this statement
 cat("Total number of incident per incident type:", "\n")
 
+
+# identify unique Incident Types in specific TPSdata file
 myIncidentTypes <- unique(TPSdata$Incident_Type)
+
 for (i in myIncidentTypes) {
-	count1 <- (sum(i == TPSdata$Incident_Type))
-	(cat('\t', i,'--', count1, '\n'))
+	# add the total number of incidents per each incident type
+	totalIncidentTypes <- (sum(i == TPSdata$Incident_Type))
+	# cat each incident type and the total number of each
+	(cat('\t', i,'--', totalIncidentTypes, '\n'))
 }
 
-
+# cat the average number of units dispatched for priority level greater than 5
 cat("Average number of units dispatched for Priority > 5:", mean(TPSdata$Units_Arrived_At_Scene[TPSdata$Priority_Number > 5]), '\n')
 
 
+# variable for sorting table of Forward_Sortation_Area in decreasing order
+maxFowardSortationArea <- sort(table(TPSdata$Forward_Sortation_Area), decreasing = TRUE)
 
-myFowardSortationArea <- table(TPSdata$Forward_Sortation_Area)
-cat("Postal code/s with the largest number of incidents (", sort(myFowardSortationArea, decreasing = TRUE)[1], '):')
-
-
-cat(rownames(sort(myFowardSortationArea, decreasing = TRUE)[1]))
 
 
 
