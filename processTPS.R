ffa49274fc9dc3adc32c7e320ab4fc6f1cf203da
Elizabeth Kim <eli.kim@mail.utoronto.ca>
2017-10-10 21:11:47 -0400

    cat total number of incidents per incident type

--- processTPS.R
+++ processTPS.R
@@ -5,6 +5,7 @@ args <- commandArgs(trailingOnly = TRUE)
 cat("Processing data from file:", args, "\n")
 
 cat("Total number of incident per incident type:", "\n")
+
 # load csv file into data frame
 TPSdata <- read.csv(args)
 
@@ -14,19 +15,4 @@ for (i in myIncidentTypes) {
 	(cat('\t', i,'--', count1, '\n'))
 }
 
-cat("Average number of units dispatched for Priority > 5:", mean(TPSdata$Units_Arrived_At_Scene[TPSdata$Priority_Number > 5]), '\n')
-cat("Postal code/s with the largest number of incidents")
 
-
-
-#count1 <- (sum(i == myIncidentTypes))
-#cat('count1')
-
-#cat("Unique data frame ----------\n")
-
-#for (elem in unique(data$Incident_Type)) {
-	#print(length(which(elem == data$Incident_Type)))
-#}
-#cat("Types are", a, "\n")
-#unique(data)
-#data[1:10, "Incident_Type"]
