70b807cf0e5a892de6990fd37c4061e5cfb29867
Elizabeth Kim <eli.kim@mail.utoronto.ca>
2017-10-10 21:58:30 -0400

    script to loop through all csv files and do processTPS.R

---
+++ processALLyears.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for i in *csv; do 
+	Rscript processTPS.R $i 
+	echo ""   
+done
--- processTPS.R
+++ processTPS.R
@@ -4,10 +4,13 @@
 args <- commandArgs(trailingOnly = TRUE)
 cat("Processing data from file:", args, "\n")
 
-cat("Total number of incident per incident type:", "\n")
 
 # load csv file into data frame
 TPSdata <- read.csv(args)
+
+
+
+cat("Total number of incident per incident type:", "\n")
 
 myIncidentTypes <- unique(TPSdata$Incident_Type)
 for (i in myIncidentTypes) {
@@ -15,8 +18,16 @@ for (i in myIncidentTypes) {
 	(cat('\t', i,'--', count1, '\n'))
 }
 
+
 cat("Average number of units dispatched for Priority > 5:", mean(TPSdata$Units_Arrived_At_Scene[TPSdata$Priority_Number > 5]), '\n')
 
+
+
+myFowardSortationArea <- table(TPSdata$Forward_Sortation_Area)
+cat("Postal code/s with the largest number of incidents (", sort(myFowardSortationArea, decreasing = TRUE)[1], '):')
+
+
+cat(rownames(sort(myFowardSortationArea, decreasing = TRUE)[1]))
 
 
 
